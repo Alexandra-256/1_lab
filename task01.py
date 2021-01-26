c9ab85062403f5ef711c7ee067046fb12b158bf3
Alexandra-256 <76553511+Alexandra-256@users.noreply.github.com>
2021-01-26 10:05:33 +0500

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Написание небольшой программы
+
+
+from random import random
+
+n = random() * 900 + 100
+n = int(n)
+print(n)
+
+a = n // 100
+b = (n // 10) % 10
+c = n % 10
+
+print(a + b + c)
